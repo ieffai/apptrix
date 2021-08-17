d47d4dededc9adcd2cfac46ebde24f0261df645e
Sergey <4923920@gmail.com>
2021-08-17 05:05:09 +0300

    removed axios requests

--- src/redux/actions/issueAction.js
+++ src/redux/actions/issueAction.js
@@ -1,55 +1,37 @@
-import { $authHost } from '../../http/index';
 import { setIssues, setProjectNames, setNewWorkItem } from '../reducers/issueReducer';
+import { fetchAllService, searchService, getWorkItemService } from '../../http/issueApi';
 
 export const fetchIssues = () => {
-  return async (dispatch) => {
-    try {
-      const { data } = await $authHost.get('/issues', {
-        params: {
-          fields: 'id,summary,project(name)',
-        },
-      });
+  return (dispatch) => {
+    fetchAllService().then((data) => {
       dispatch(setIssues(data));
       let names = [];
       data.map((item) => names.push(item.project.name));
       let filteredNames = Array.from(new Set(names));
       dispatch(setProjectNames(filteredNames));
-    } catch (error) {
-      console.log(error.response?.data?.message);
-    }
+    });
   };
 };
 
 export const searchIssues = (search) => {
-  return async (dispatch) => {
-    try {
-      const { data } = await $authHost.get('/issues', {
-        params: {
-          fields: 'id,summary,project(name)',
-          query: `project:${search}`,
-        },
-      });
+  return (dispatch) => {
+    searchService(search).then((data) => {
       dispatch(setIssues(data));
-    } catch (error) {
-      console.log(error.response?.data?.message);
-    }
+    });
   };
 };
 
 export const getWorkItem = (id) => {
-  return async (dispatch) => {
-    try {
-      let { data } = await $authHost.get(`/workItems/128-${id}`, {
-        params: {
-          fields: 'id,author(name),duration(presentation),creator(name),updated',
-        },
-      });
-
-      data.name = data.author.name;
-      data.duration = data.duration.presentation;
-      dispatch(setNewWorkItem(data));
-    } catch (error) {
-      console.log(error.response?.data?.message);
-    }
+  return (dispatch) => {
+    getWorkItemService(id).then((data) => {
+      if (data) {
+        data.name = data.author.name;
+        data.duration = data.duration.presentation;
+        dispatch(setNewWorkItem(data));
+      } else {
+        let noUser = { name: 'no such user', duration: 'no time avaliable' };
+        dispatch(setNewWorkItem(noUser));
+      }
+    });
   };
 };
--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -1,53 +1,30 @@
 import { setUser, setUsers } from '../reducers/userReducer';
-import { $host, $authHost } from '../../http/index';
+import { fetchUsersService, loginService, logoutService, refreshService } from '../../http/userApi';
 
-export const login = (username, password) => {
-  return async (dispatch) => {
-    try {
-      const response = await $host.post('/', { username, password });
-      localStorage.setItem('access', response.data.access);
-      localStorage.setItem('refresh', response.data.refresh);
-      dispatch(setUser(true));
-    } catch (error) {
-      console.log(error.response?.data?.message);
-    }
+export const loginAction = (username, password) => {
+  return (dispatch) => {
+    loginService(username, password);
+    dispatch(setUser(true));
   };
 };
 
-export const refreshToken = () => {
-  return async (dispatch) => {
-    try {
-      const response = await $host.post('/refresh/', {
-        refresh: localStorage.getItem('refresh'),
-      });
-      localStorage.setItem('access', response.data.access);
-      dispatch(setUser(true));
-    } catch (error) {
-      console.log(error.response?.data?.message);
-    }
+export const refreshAction = () => {
+  return (dispatch) => {
+    refreshService();
+    dispatch(setUser(true));
   };
 };
 
 export const logout = () => {
-  return async (dispatch) => {
-    localStorage.removeItem('access');
-    localStorage.removeItem('refresh');
+  return (dispatch) => {
+    logoutService();
     dispatch(setUser(false));
     dispatch(setUsers([]));
   };
 };
 
 export const fetchUsers = () => {
-  return async (dispatch) => {
-    try {
-      const { data } = await $authHost.get('/users', {
-        params: {
-          fields: 'id,name,login,email',
-        },
-      });
-      dispatch(setUsers(data));
-    } catch (error) {
-      console.log(error.response?.data?.message);
-    }
+  return (dispatch) => {
+    fetchUsersService().then((data) => dispatch(setUsers(data)));
   };
 };
