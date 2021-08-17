44ccadf5cba2651a58dba6e6d1d989931afae006
Sergey <4923920@gmail.com>
2021-08-17 05:05:56 +0300

    removed bearer token to env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const AUTH_API_URL = 'http://erp.apptrix.ru/api/token/';
 const USERS_API_URL = 'https://demo-apptrix.myjetbrains.com/youtrack/api/';
-const AUTH_BEARER = 'perm:cm9vdA==.NDktMQ==.LFQD3WeI6Jr9SyKbN02s924HLyScsb';
+const AUTH_BEARER = process.env.REACT_APP_ACCESS_TOKEN;
 const AUTH_ROUTE = '/auth';
 const HOME_ROUTE = '/home';
 const TASKS_ROUTE = '/tasks';
